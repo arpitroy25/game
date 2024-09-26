31363bdde13f68d9bde2db06d21ca7de04c1b070
arpitroy25 <arpitroy2225@gmail.com>
2024-09-26 22:24:52 +0530

    Add files via upload

---
+++ game.c
@@ -0,0 +1,274 @@
+#include <stdio.h>
+
+int main() {
+    char answer;
+    int score = 0;
+
+    // Question 1
+    printf("Question 1: The sun rises in the east. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 2
+    printf("Question 2: The earth is flat. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 3
+    printf("Question 3: Water boils at 100 degrees Celsius. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 4
+    printf("Question 4: Humans breathe in carbon dioxide. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+     // Question 5
+    printf("Question 5:Australia is a country. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+
+ // Question 6
+    printf("Question 6: Sharks are mammals. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+
+ // Question 7
+    printf("Question 7: Pacific ocean is largest ocean on the planet. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 8
+    printf("Question 8: Atlantic ocean is largest ocean on the planet. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 9
+    printf("Question 9: New Delhi is the capital of India. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 10
+    printf("Question 10: Taj Mahal is located in New Delhi. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 11
+    printf("Question 11: The sum of 2+3=6. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 12
+    printf("Question 12: Rain comes from Clouds . (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 13
+    printf("Question 13:There are 206 Bones in Human Body. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 14
+    printf("Question 14: Cape Town is located in Vietnam. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 15
+    printf("Question 15:Pluto is a Dwarf Planet. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 16
+    printf("Question 16: The Yamuna is the main tributary of the River Gangas in India. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 17
+    printf("Question 17: The Human Body contains Four Lungs. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 18
+    printf("Question 18: Molecules are smaller than Electron. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 19
+    printf("Question 19: Mount K2 is the largest Mountain Peak in the world. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 20
+    printf("Question 20: Venice is an Italian city known for its Canals. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 21
+    printf("Question 21: Mars is the closest Planet from the Sun. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 22
+    printf("Question 22: Protons have Negative Charge whereas Electron have Positive charge. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+     // Question 23
+    printf("Question 23: Square Root of 29 is 831 . (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'F' || answer == 'f') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 24
+    printf("Question 24: Is Mucus Present in Stomach . (Y/N): ");
+    scanf(" %c", &answer);
+    if (answer == 'Y' || answer == 'y') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+    // Question 25
+    printf("Question 25: Formula of Momentum is Mass*Velocity. (T/F): ");
+    scanf(" %c", &answer);
+    if (answer == 'T' || answer == 't') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect!\n");
+    }
+
+
+    
+    // Final score
+    printf("\nYour final score is: %d out of 25\n", score);
+
+    // Feedback based on score
+    if (score == 25) {
+        printf("Excellent work!\n");
+    } else if (score >= 1) {
+        printf("Good job!\n");
+    } else {
+        printf("Better luck next time!\n");
+    }
+
+    return 0;
+}
